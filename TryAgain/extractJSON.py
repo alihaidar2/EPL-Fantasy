f8af578760c5febbcca04fec12cb9a4ae2b5a477
Ali Haidar <ahaid090@uottawa.ca>
2020-11-10 17:46:32 -0500

    cleaned and exported fixture data

--- TryAgain/extractJSON.py
+++ TryAgain/extractJSON.py
@@ -4,6 +4,11 @@ import json
 import pandas as pd
 import numpy as np
 
+# printing options
+# pd.set_option('display.max_rows', 500)
+# pd.set_option('display.max_columns', 500)
+# pd.set_option('display.width', 1000)
+
 # get data from FPL
 url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 r = requests.get(url)
@@ -35,16 +40,53 @@ teams_df = teams_df.rename(columns={"id" : "TeamId"})
 # mean values for each team
 team_pivot = slim_elements_df.pivot_table(index='team',values='value',aggfunc=np.mean).reset_index()
 
-# dataframes for each position: can use them for histograms
-# fwd_df = slim_elements_df.loc[slim_elements_df.position == 'Forward']
-# mid_df = slim_elements_df.loc[slim_elements_df.position == 'Midfielder']
-# def_df = slim_elements_df.loc[slim_elements_df.position == 'Defender']
-# goal_df = slim_elements_df.loc[slim_elements_df.position == 'Goalkeeper']
-
 # reorder and export to csv
 slim_elements_df = slim_elements_df[['second_name', 'team', 'position', 'now_cost', 'total_points', 'value', 'minutes', 'transfers_in', 'selected_by', 'goals_scored', 'assists', 'goals_conceded', 'saves']]
 teams_df = teams_df[['TeamId', 'name', 'short_name','strength','strength_overall_home','strength_overall_away','strength_attack_home',	'strength_attack_away',	'strength_defence_home','strength_defence_away','pulse_id', 'code']]
 slim_elements_df.to_csv('/Users/alihaidar/Projects/Python/fantasyepl/TryAgain/fpl_players.csv', index=True, index_label="PlayerId")
 teams_df.to_csv('/Users/alihaidar/Projects/Python/fantasyepl/TryAgain/fpl_teams.csv', index=False)
 
-# print(elements_types_df.head())
+matches = pd.read_csv('/Users/alihaidar/Projects/Python/fantasyepl/MvcFantasyClean/matches_final.csv')
+matches_sorted = matches.sort_values(['Matchday'])
+matches_sorted = matches_sorted.replace(to_replace = ' FC', value = '', regex = True)
+
+teams = [ "Burnley","Manchester United","Manchester City","Aston Villa",
+            "Fulham", "Arsenal", "Crystal Palace", "Southampton",
+            "Liverpool", "Leeds United", "West Ham United", "Newcastle United",
+            "West Bromwich Albion", "Leicester City", "Tottenham Hotspur", "Everton",
+            "Sheffield United", "Wolverhampton Wanderers", "Brighton & Hove Albion","Chelsea" ]
+
+## CREATE DATAFRAME AND USE THIS ALGORITHM TO POPULATE IT
+## YOU SHOULD JUST HAVE TO CHANGE THE LINES WITH i FOR INITIAL CELL
+## THEN PRINT WITH GAMEWEEK CELLS
+
+fpl_fixtures_df = pd.DataFrame(
+    columns=["Team", 
+        "GW1","GW2","GW3","GW4","GW5","GW6","GW7","GW8","GW9","GW10",
+        "GW11","GW12","GW13","GW14","GW15","GW16","GW17","GW18","GW19","GW20",
+        "GW21","GW22","GW23","GW24","GW25","GW26","GW27","GW28","GW29","GW30",
+        "GW31","GW32","GW33","GW34","GW35","GW36","GW37","GW38"])
+
+# print("Empty Dataframe ", fpl_fixtures_df, sep='\n')
+
+allGames = []
+j = 0
+
+## GOES THROUGH TEAMS AND GETS 
+for i in teams :
+    teamGames = []
+    games = matches_sorted[matches_sorted.apply(lambda row: row.str.contains(str(i), case=False).any(), axis=1)]
+    print(i)
+    teamGames.append(i)
+    for index, row in games.iterrows() :
+        if row['Home'] == str(i) :
+            teamGames.append(row['Away'] + ' (Home) ' + str(row['Date']))
+        else :
+            teamGames.append(row['Home'] + ' (Away) ' + str(row['Date']))
+    j += 1
+    print(teamGames, j)
+    fpl_fixtures_df.loc[j] = np.array(teamGames)
+
+print(fpl_fixtures_df.head(5))
+
+fpl_fixtures_df.to_csv('/Users/alihaidar/Projects/Python/fantasyepl/TryAgain/fpl_fixtures.csv', index=False)
